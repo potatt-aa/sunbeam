fbf62a612586bec29935d7df9d79f0d969230252
potatt_aa <alexis.lee730@gmail.com>
2026-08-29 15:12:24 -0400

    ou shii it works

--- larper/style.js
+++ larper/style.js
@@ -1,11 +1,1 @@
-const button = document.querySelector("#doubloons");
-const count = document.querySelector("#count");
-
-let doubloons = 0;
-
-button.addEventListener("click", () => {
-    doubloons = doubloons + 1;
-    count.textContent = doubloons + " doubloons";
-});
-
 
